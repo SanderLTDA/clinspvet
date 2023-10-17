356f57d64eeedc3c3dd082fb118cd682de198cdc
Marcelo <ma.sander11@gmail.com>
2023-10-16 23:27:23 -0300

    fix google maps link

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,5 @@
 export const Map: React.FC = () => {
-    const mapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7311.331693898018!2d-46.64112377247911!3d-23.616314058323763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a51aad099a1%3A0xf036bbd3894d96a4!2sAv.%20Jabaquara%20-%20Mirand%C3%B3polis%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004045-002!5e0!3m2!1spt-BR!2sbr!4v1678658862751!5m2!1spt-BR!2sbr";
+    const mapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1579.4615981741508!2d-46.63977052439393!3d-23.61717999603525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5be1bac2bb75%3A0xec1256fde29946d6!2sClinSP%20Vet%20-%20Cl%C3%ADnica%20Veterin%C3%A1ria!5e0!3m2!1spt-BR!2sus!4v1697509348609!5m2!1spt-BR!2sus%22%20width=%22600%22%20height=%22450%22%20style=%22border:0;%22%20allowfullscreen=%22%22%20loading=%22lazy%22%20referrerpolicy=%22no-referrer-when-downgrade";
 
     return (
         <iframe
